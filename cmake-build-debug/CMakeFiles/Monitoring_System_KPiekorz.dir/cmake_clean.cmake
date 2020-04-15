39e5127700ebf684cd0641c12ae19138585f8cc6
KPiekorz <kpiekorz919@protonmail.com>
2020-04-15 23:36:00 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/Monitoring_System_KPiekorz.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Monitoring_System_KPiekorz.dir/main.cpp.obj"
+  "Monitoring_System_KPiekorz.exe"
+  "Monitoring_System_KPiekorz.exe.manifest"
+  "Monitoring_System_KPiekorz.pdb"
+  "libMonitoring_System_KPiekorz.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Monitoring_System_KPiekorz.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
