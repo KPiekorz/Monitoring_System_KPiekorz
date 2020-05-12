05b010aa099900c28c42bc4c4aed15a4f2b1e352
KPiekorz <kpiekorz919@protonmail.com>
2020-05-12 02:02:16 +0200

    mala zmiana

--- scr/main.cpp
+++ scr/main.cpp
@@ -6,24 +6,7 @@
 using namespace std;
 
 int main() {
-
-//    TimeHistory <double> time_trzy(0.01, 1, "Volt", 0.01);
-//    TimeHistory <double> time_jeden(0.01, 2, "Volt", 0.01);
-//
-//    gauss_generator(3., 0.3, time_trzy, 4096);
-//    gauss_generator(1., 0.3, time_jeden, 4096);
-//
-//    cout << "RMS: " << calculate_RMS(time_trzy) << endl;
-//
-//    time_trzy << "time_trzy.txt";
-//    time_jeden << "time_jeden.txt";
-//
-//    Spectrum<double> widmo_z_trzy;
-//
-//    calculate_FFT(time_trzy, widmo_z_trzy);
-//
-//    widmo_z_trzy << "widma_z_trzy.txt";
-
+    
     // Inicjalizacja sesji pomiarowej
     TimeHistory<double> sygnal1(0.01, 1, "Dev1", 0.01);
     TimeHistory<double> sygnal2(0.01, 1, "Dev2", 0.01);
